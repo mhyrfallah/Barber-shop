c87fd7f1f7a7096529627f64cdded78db851599f
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-08-20 19:59:54 +0330

    create admin file and super user

--- barbershop/admin.py
+++ barbershop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Day, TimeSlot, Reservation
 
-# Register your models here.
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('date', 'is_available')
+    list_filter = ('is_available',)
+    ordering = ('date',)
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('day', 'start_time', 'price', 'is_booked')
+    list_filter = ('day__date', 'is_booked')
+    ordering = ('day', 'start_time')
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'slot', 'is_paid', 'created_at')
+    list_filter = ('is_paid',)
